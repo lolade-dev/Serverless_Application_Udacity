5f97b26eec8dffb683a56ea1266f0628e095effb
Ololade Olanrewaju <Oolarenwaju@wragbysolutions.com>
2022-08-20 18:51:40 +0100

    Renamed repo

---
+++ backend/src/lambda/http/deleteTodo.ts
@@ -0,0 +1,25 @@
+import 'source-map-support/register'
+
+import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+
+import { deleteToDo } from '../../helpers/todos'
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    // TODO: Remove a TODO item by id
+    console.log("Processing Event ", event);
+    const authorization = event.headers.Authorization;
+    const split = authorization.split(' ');
+    const jwtToken = split[1];
+
+    const todoId = event.pathParameters.todoId;
+
+    const deleteData = await deleteToDo(todoId, jwtToken);
+
+    return {
+        statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+        },
+        body: deleteData,
+    }
+};
